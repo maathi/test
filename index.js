39c0a0327c6c365eda4dd6ad80caa4860e8cd046
maathi <mahdi221b@gmail.com>
2021-06-22 16:38:49 +0100

    filtering

--- index.js
+++ index.js
@@ -6,17 +6,41 @@ if (!shell.which("git")) {
 }
 
 let one, two
-const path = "cute-dots"
+const user = "maathi"
+const repo = "cute-dots"
+const dirs = [
+  "polybar",
+  "rofi",
+  "bspwm",
+  "i3",
+  "lf",
+  "ranger",
+  "zsh",
+  "awesome",
+  "dwm",
+  "mpv",
+  "alacritty",
+  "zathura",
+  "dunst",
+  "picom",
+]
 // shell.cd(path)
 
-shell.exec("git clone https://github.com/maathi/cute-dots")
+shell.exec(`git clone https://github.com/${user}/${repo}`)
 // let a = shell.exec(`find ${path} -regex .*\.sh`)
+// one = Date.now()
+// let find1 = shell.exec(`find ${repo} -type d -name polybar`)
+// two = Date.now()
+// console.log(find1, two - one)
+console.log("********************")
 one = Date.now()
-let find1 = shell.exec(`find ${path} -name polybar`)
-two = Date.now()
-console.log(find1, two - one)
+// let find2 = shell.find(repo)
+let paths = shell.find(repo)
 
-one = Date.now()
-let find2 = shell.find(path).filter((f) => f.match(/.*polybar$/))
+// let found = paths.filter((f) => f.match(/.*polydbar$/))
+
+let newDirs = dirs.filter((d) =>
+  paths.find((f) => f.match(new RegExp(`./*${d}$`)))
+)
 two = Date.now()
-console.log(find2, two - one)
+console.log(">>>>>>>>>>>", newDirs, two - one)
